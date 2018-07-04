6b76a11a7b17cff19e7752627524832e7c11b97e
Alex Lewis <alew46@gmail.com>
2018-07-04 18:28:21 -0400

    Checkpoint 7: completed

--- src/components/Album.js
+++ src/components/Album.js
@@ -9,8 +9,43 @@ class Album extends Component {
       return album.slug === this.props.match.params.slug
     });
 
-    this.state = { album: album};
+    this.state = {
+      album: album,
+      currentSong: album.songs[0],
+      isPlaying: false
+    };
+
+    this.audioElement = document.createElement('audio');
+    this.audioElement.src = this.state.album.songs[0].audioSrc
   }
+
+  play() {
+    this.audioElement.play();
+    this.setState( {isPlaying: true} )
+  };
+
+  pause() {
+    this.audioElement.pause();
+    this.setState( {isPlaying: false} )
+  };
+
+  setSong(song) {
+    this.audioElement.src = song.audioSrc;
+    this.setState( {currentSong: song} );
+  };
+
+  handleSongClick(song) {
+    var isSameSong = this.state.currentSong === song;
+
+    if (this.state.isPlaying && isSameSong) {
+      this.pause();
+    } else {
+      if (!isSameSong) {
+        this.setSong(song);
+      }
+      this.play();
+    }
+  };
 
 
   render() {
@@ -31,7 +66,7 @@ class Album extends Component {
           <tbody>
             {
               this.state.album.songs.map( (song, index) =>
-                <tr key={index}>
+                <tr className="song" key={index} onClick={ () => this.handleSongClick(song) }>
                   <td>{index + 1}</td>
                   <td>{song.title}</td>
                   <td>{song.duration}</td>
